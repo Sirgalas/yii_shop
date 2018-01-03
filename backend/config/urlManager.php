f1dabb6e86734e98bb6c1b2eb26073a371def5de
sirgalas <hudos@rambler.ru>
2018-01-03 21:37:54 +0300

    add more file

--- backend/config/urlManager.php
+++ backend/config/urlManager.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 02.01.18
- * Time: 22:19
- */
+/** @var array $params */
+return [
+    'class'=>'yii\web\UrlManager',
+    'hostInfo'=>$params['backendHostInfo'],
+    'enablePrettyUrl' => true,
+    'showScriptName' => false,
+    'rules' => [
+        '' => 'site/index',
+        '<_a:login|logout>' => 'site/<_a>',
+        '<_c:[\w\-]+>' => '<_c>/index',
+        '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+        '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
+        '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
+    ],
+]; ?>
--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 02.01.18
- * Time: 22:24
- */
+/** @var array $params */
+return  [
+    'class'=>'yii\web\UrlManager',
+    'hostInfo'=>$params['frontendHostInfo'],
+    'enablePrettyUrl' => true,
+    'showScriptName' => false,
+    'rules' => [
+        '' => 'site/index',
+        '<_a:login|logout>' => 'site/<_a>',
+        '<_c:[\w\-]+>' => '<_c>/index',
+        '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+        '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
+        '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
+    ],
+]; ?>
